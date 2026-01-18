f3e6109f322b0967b612fb9e594157175e18d9cd
Bimaghira <bimaghira02@gmail.com>
2026-01-18 12:33:14 +0700

    Delete SISTEM MANAJEMEN TRANSAKSI SMART-CAFE BERBASIS LOYALTY POINT MEMBER directory

--- SISTEM MANAJEMEN TRANSAKSI SMART-CAFE BERBASIS LOYALTY POINT MEMBER/src/Produk.java
+++
@@ -1,7 +0,0 @@
-class Produk {
-    private String nama;
-    private double harga;
-    public Produk(String n, double h) { this.nama = n; this.harga = h; }
-    public String getNama() { return nama; }
-    public double getHarga() { return harga; }
-}
