693d3c3fe1ca1c1cb967c6134752c9fcf4dd2c1b
Bimaghira <bimaghira02@gmail.com>
2026-01-18 12:27:57 +0700

    Delete projekAkhirPBO directory

--- projekAkhirPBO/src/MemberGold.java
+++
@@ -1,5 +0,0 @@
-class MemberGold extends Member {
-    public MemberGold(String n) { super(n); }
-    @Override public String getTipe() { return "Gold Member"; }
-    @Override public int hitungPoin(double total) { return (int) (total / 10000) * 2; }
-}
--- projekAkhirPBO/src/MemberSilver.java
+++
@@ -1,5 +0,0 @@
-class MemberSilver extends Member {
-    public MemberSilver(String n) { super(n); }
-    @Override public String getTipe() { return "Silver Member"; }
-    @Override public int hitungPoin(double total) { return (int) (total / 10000); }
-}
