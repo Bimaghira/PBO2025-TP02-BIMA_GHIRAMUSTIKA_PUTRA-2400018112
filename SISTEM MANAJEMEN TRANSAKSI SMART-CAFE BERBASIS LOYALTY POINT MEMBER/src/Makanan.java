35de44eb8019fc973f4b6a2d98303c24c78d2ba7
Bimaghira <bimaghira02@gmail.com>
2026-01-18 12:28:41 +0700

    Add files via upload

---
+++ SISTEM MANAJEMEN TRANSAKSI SMART-CAFE BERBASIS LOYALTY POINT MEMBER/src/Makanan.java
@@ -0,0 +1,13 @@
+class Makanan extends Produk {
+        public Makanan(String nama, double harga) {
+            super(nama, harga);
+        }
+    }
+
+    // Sub-kelas: Minuman
+    class Minuman extends Produk {
+        public Minuman(String nama, double harga) {
+            super(nama, harga);
+        }
+    }
+
